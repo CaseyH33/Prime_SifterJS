8d297398b2d32e170a3e630f71db54403664ef99
Casey Heitz & Jordan Johansen <student@epicodus.com>
2015-09-09 16:10:18 -0700

    Refactor

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,15 @@
 var primeSifter = function(number) {
   var numArray = numberList(number);
   var prime = 2;
+  var count = 0;
   while (prime <= number) {
     numArray.forEach(function(num) {
       if ((num !== prime) && (num % prime === 0)) {
         numArray.splice(numArray.indexOf(num), 1);
       }
     });
-    prime++;
+    count++;
+    prime = numArray[count];
   }
   return numArray;
 }
@@ -19,3 +21,16 @@ var numberList = function(number) {
   }
   return numList;
 }
+
+$(function() {
+  $("form#sift").submit(function(event) {
+    var number = parseInt($("input#number").val());
+    var result = primeSifter(number);
+    $(".input_number").empty();
+    $(".output").empty();
+    $(".input_number").text(number);
+    $(".output").text(result.join(", "));
+    $("#result").show();
+    event.preventDefault();
+  });
+});
