839fc2ddbcea7076e4db10c7aa87d2668e8ed6e8
Casey Heitz & Jordan Johansen <student@epicodus.com>
2015-09-09 15:15:29 -0700

    Add passing test for returning all prime numbers up to input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 var primeSifter = function(number) {
   var numArray = numberList(number);
+  var prime = 2;
+  while (prime <= number) {
+    numArray.forEach(function(num) {
+      if ((num !== prime) && (num % prime === 0)) {
+        numArray.splice(numArray.indexOf(num), 1);
+      }
+    });
+    prime++;
+  }
+  return numArray;
 }
 
 var numberList = function(number) {
--- spec/specs.js
+++ spec/specs.js
@@ -1,16 +1,15 @@
-describe('primeSifter', function() {
-  // it("determines whether or not an inputted number is prime", function() {
-  //   expect(primeSifter(17)).to.equal(true);
-  // });
-  //
-  // it("determines whether or not an inputted number is prime", function() {
-  //   expect(primeSifter(4)).to.equal(false);
-  // });
-
+describe('numberList', function() {
   it("creates an array of all numbers from 2 to the inputted number", function() {
     expect(numberList(5)).to.eql([2,3,4,5]);
   });
+});
 
-  
+describe('primeSifter', function() {
+  it("returns an array of all prime numbers up to inputted number", function() {
+    expect(primeSifter(12)).to.eql([2,3,5,7,11]);
+  });
 
+  it("doesn't f$&# up when it ends on a prime number", function() {
+    expect(primeSifter(7)).to.eql([2,3,5,7]);
+  });
 });
